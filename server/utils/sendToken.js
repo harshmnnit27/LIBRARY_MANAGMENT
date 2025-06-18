fc9087fcde403f5659892b6cf955b3aa193c6770
Ayush5071 <ayusht5071@gmail.com>
2025-06-19 02:33:17 +0530

    fix: robustly parse COOKIE_EXPIRE for cookie expiration

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,8 +1,9 @@
 export const sendToken=(user,statusCode,message,res)=>{
     const token = user.generateToken();
+    const days = parseInt(process.env.COOKIE_EXPIRE) || 3;
     res.status(statusCode).cookie("token",token,{
         expires:new Date(
-            Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000
+            Date.now()+days*24*60*60*1000
         ),
         httpOnly:true,
     })
