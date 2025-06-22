790a7f039e8bc6b46d3fd6cada569ee50f812502
Your Name <pandalagovind04@gmail.com>
2025-06-22 21:29:00 +0530

    Minor bug fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,28 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': import.meta.env.VITE_BACKEND_URL,
+//     },
+//   },
+// })
+
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': import.meta.env.VITE_BACKEND_URL,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': env.VITE_BACKEND_URL,
+      },
     },
-  },
+  }
 })
+
